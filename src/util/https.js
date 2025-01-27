954f36543e0a19f0ee13cb497d72b251b8982262
Camila Guillan <62452679+camiguillan@users.noreply.github.com>
2025-01-27 14:04:46 +0000

    implementing useQuery

--- src/util/https.js
+++ src/util/https.js
@@ -1,8 +1,14 @@
 
 const path = 'https://cautious-halibut-r69xr5795762w759-3000.app.github.dev/'
 
-export async function fetchEvents() {
-    const response = await fetch(path + 'events');
+export async function fetchEvents({signal, searchTerm}) {
+    let url = path + 'events';
+
+    if(searchTerm){
+      url += '?search=' + searchTerm
+    }
+
+    const response = await fetch(path, {signal: signal});
 
     if (!response.ok) {
       const error = new Error('An error occurred while fetching the events');
