a97c500d2f69cd442870250abf38d57fa98d816f
Camila Guillan <62452679+camiguillan@users.noreply.github.com>
2025-01-28 14:24:05 +0000

    search by a certain amount of events

--- src/util/https.js
+++ src/util/https.js
@@ -4,13 +4,19 @@ import { QueryClient } from "@tanstack/react-query";
 const path = 'https://cautious-halibut-r69xr5795762w759-3000.app.github.dev/'
 export const queryClient = new QueryClient()
 
-export async function fetchEvents({ signal, searchTerm }) {
+export async function fetchEvents({ signal, searchTerm, max }) {
   console.log(searchTerm);
   let url = path + 'events';
 
-  if (searchTerm) {
+  
+  if( searchTerm && max){
+    url += '?search=' + searchTerm + '&max='  + max ;
+  }else if (searchTerm) {
     url += '?search=' + searchTerm;
+  }else  if(max){
+    url += '?max='  + max ;
   }
+
 
   const response = await fetch(url, { signal: signal });
 
